aaecd2275d6761486109391ddafb4bd098a2277a
josekameydev01 <gk01cracklitos@gmail.com>
2025-05-25 19:17:33 -0600

    Data inference and type safety

---
+++ DataInferenceAndTypeSafety.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let a = 5.5 // Double
+let b = 5.0 // Double
+let c = 5 // Int
+let d = "Nicolas" // String
+
+/*This code will result in an error.
+let name = "Carlos"
+name = 100
+*/
+
