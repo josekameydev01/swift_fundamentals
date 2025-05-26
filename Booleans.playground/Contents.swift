950ba079f5f39663a2d2b702404b0f57d3f74cf1
josekameydev01 <gk01cracklitos@gmail.com>
2025-05-26 07:07:19 -0600

    How to store booleans

---
+++ Booleans.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let a = true
+let b = false
+
+let x = 10.isMultiple(of: 2)
+let y = "Hello.png".hasSuffix(".jpg")
+let z = "Hello.jpg".hasSuffix(".jpg")
+
+let value1 = !true
+let value2 = !false
+
+var gameOver = true
+gameOver.toggle()
+gameOver.toggle()
