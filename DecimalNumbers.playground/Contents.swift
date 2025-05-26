1f6cf1640dbf8691adf531dd01c9e856df9bba92
josekameydev01 <gk01cracklitos@gmail.com>
2025-05-25 19:11:15 -0600

    How to store decimal numbers

---
+++ DecimalNumbers.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let number = 3.5
+
+/*This code will result in an error.
+let a = 5.0
+let b = 1
+let c = a + b
+*/
+
+let a = 5.5
+let b = 1
+let c = Int(a) + b
+
+let x = 5.5
+let y = 1
+let z = Double(y) + x
+
