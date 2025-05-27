6a79a2f82bb60f0bc03f7bd484e2cfc52cc90d7f
josekameydev01 <gk01cracklitos@gmail.com>
2025-05-26 21:28:50 -0600

    Checkpoint 1 solved

---
+++ Checkpoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let celsiusTemperature = 17.0
+let fahrenheitTemperature = ((celsiusTemperature * 9) / 5) + 32
+
+print("The temperature in New York in degrees celsius is \(celsiusTemperature)°C")
+print("The temperature in New York in degress fharenheit is \(fahrenheitTemperature)°F")
