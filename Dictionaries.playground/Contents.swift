fad15fed31fcfb040ca13f73b5a88497cd3b5a14
josekameydev01 <gk01cracklitos@gmail.com>
2025-05-27 21:10:41 -0600

    How to store data in dictionaries

---
+++ Dictionaries.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var employee = [
+    "name": "Carlos",
+    "job": "Developer",
+    "location": "Mexico"
+]
+
+employee["name"]
+employee["job"]
+employee["location"]
+employee["age"]
+employee["age", default: "Unknown"]
+
+var olympics = [String: Int]()
+olympics["London"] = 2012
+olympics["Rio de Janeiro"] = 2016
+olympics["Tokyo"] = 2021
+
+var olympics2 = Dictionary<String, Int>()
+olympics2["London"] = 2012
+olympics2["Rio de Janeiro"] = 2016
+olympics2["Tokyo"] = 2021
+olympics2["Tokyo"] = 2025
+
+employee.count
+employee.removeValue(forKey: "location")
+employee
+employee.removeAll()
+
+olympics.keys
+olympics.values
