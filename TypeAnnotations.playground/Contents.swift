6c87f3f712317ee64a81cd0a5cc75e3e5c89a08a
josekameydev01 <gk01cracklitos@gmail.com>
2025-05-29 11:51:29 -0600

    How to use type annotations

---
+++ TypeAnnotations.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let username: String = "Lasso"
+var score: Int = 0
+
+var score1: Double = 3
+
+enum UIStyle {
+    case light, dark
+}
+
+var currentUIStyle: UIStyle
+currentUIStyle = .dark
+currentUIStyle = .light
+
+var points: Int
+points = 100
+points = 200
+
+let user: String
+//print(user) <- error.
+user = "Carlos"
+print(user)
+
+var user1: String
+//print(user1) Error
+user1 = "Cracklitos"
+print(user1)
