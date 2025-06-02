7897a66eb84064af6b84fc5d7921f4f5e62addf1
josekameydev01 <gk01cracklitos@gmail.com>
2025-06-01 19:48:26 -0600

    How to skip loop items

---
+++ BreakContinue.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if !filename.hasSuffix(".jpg") {
+        continue
+    }
+    print("Found picture: \(filename)")
+}
+print()
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+    }
+    
+    if multiples.count == 10 {
+        break
+    }
+}
+print(multiples)
